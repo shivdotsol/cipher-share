df61b01753782dc3806a90d58cf0b61261fb83ef
shivdotsol <defi.shiv@gmail.com>
2025-08-25 22:25:11 +0530

    redis url changed

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,8 +1,8 @@
 import Redis from "ioredis";
 
 const redis = new Redis({
-    host: "redis-14163.c301.ap-south-1-1.ec2.redns.redis-cloud.com",
-    port: 14163,
+    host: "redis-13490.crce206.ap-south-1-1.ec2.redns.redis-cloud.com",
+    port: 13490,
     username: "default",
     password: process.env.REDIS_PASSWORD,
 });
